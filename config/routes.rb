5b53426f330d2218e58b00660e05f1bf5b6a43d1
Brennan Roel <brennanroel@gmail.com>
2020-08-23 20:06:20 -0500

    signup working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  root "sessions#home"
+  
+  #sign up route
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  #login route
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  #logout route
+  delete '/logout' => 'sessions#destroy'
+
+  
   resources :comments
   resources :movies
   resources :lists
